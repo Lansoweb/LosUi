e6f30be3321e8032eeafdef2a9671869c9651482
Leandro Silva <lansoweb@hotmail.com>
2015-01-18 10:48:40 -0200

    Using the LosUrl inside the paginator

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,105 +1,31 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/Cliente for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
-$additionalNamespaces = $additionalModulePaths = $moduleDependencies = null;
+use LosLicenseTest\ServiceManagerTestCase;
 
-$rootPath = realpath(dirname(__DIR__));
-$testsPath = "$rootPath/tests";
+ini_set('error_reporting', E_ALL);
 
-if (is_readable($testsPath.'/TestConfiguration.php')) {
-    require_once $testsPath.'/TestConfiguration.php';
-} else {
-    require_once $testsPath.'/TestConfiguration.php.dist';
-}
+$files = array(__DIR__ . '/../vendor/autoload.php', __DIR__ . '/../../../autoload.php');
 
-$path = array(
-    ZF2_PATH,
-    get_include_path(),
-);
-set_include_path(implode(PATH_SEPARATOR, $path));
+foreach ($files as $file) {
+    if (file_exists($file)) {
+        $loader = require $file;
 
-require_once 'Zend/Loader/AutoloaderFactory.php';
-require_once 'Zend/Loader/StandardAutoloader.php';
-
-use Zend\Loader\AutoloaderFactory;
-use Zend\Loader\StandardAutoloader;
-
-// setup autoloader
-AutoloaderFactory::factory(
-    array(
-        'Zend\Loader\StandardAutoloader' => array(
-            StandardAutoloader::AUTOREGISTER_ZF => true,
-            StandardAutoloader::ACT_AS_FALLBACK => false,
-            StandardAutoloader::LOAD_NS => $additionalNamespaces,
-        ),
-    )
-);
-
-// The module name is obtained using directory name or constant
-$moduleName = pathinfo($rootPath, PATHINFO_BASENAME);
-if (defined('MODULE_NAME')) {
-    $moduleName = MODULE_NAME;
-}
-
-// A locator will be set to this class if available
-$moduleTestCaseClassname = '\\'.$moduleName.'Test\\Framework\\TestCase';
-
-// This module's path plus additionally defined paths are used $modulePaths
-$modulePaths = array(dirname($rootPath));
-if (isset($additionalModulePaths)) {
-    $modulePaths = array_merge($modulePaths, $additionalModulePaths);
-}
-
-// Load this module and defined dependencies
-$modules = array($moduleName);
-if (isset($moduleDependencies)) {
-    $modules = array_merge($modules, $moduleDependencies);
-}
-
-$listenerOptions = new Zend\ModuleManager\Listener\ListenerOptions(array('module_paths' => $modulePaths));
-$defaultListeners = new Zend\ModuleManager\Listener\DefaultListenerAggregate($listenerOptions);
-$sharedEvents = new Zend\EventManager\SharedEventManager();
-$moduleManager = new \Zend\ModuleManager\ModuleManager($modules);
-$moduleManager->getEventManager()->setSharedManager($sharedEvents);
-$moduleManager->getEventManager()->attachAggregate($defaultListeners);
-$moduleManager->loadModules();
-
-if (method_exists($moduleTestCaseClassname, 'setLocator')) {
-    $config = $defaultListeners->getConfigListener()->getMergedConfig();
-
-    $di = new \Zend\Di\Di();
-    $di->instanceManager()->addTypePreference('Zend\Di\LocatorInterface', $di);
-
-    if (isset($config['di'])) {
-        $diConfig = new \Zend\Di\Config($config['di']);
-        $diConfig->configure($di);
+        break;
     }
-
-    $routerDiConfig = new \Zend\Di\Config(
-        array(
-            'definition' => array(
-                'class' => array(
-                    'Zend\Mvc\Router\RouteStackInterface' => array(
-                        'instantiator' => array(
-                            'Zend\Mvc\Router\Http\TreeRouteStack',
-                            'factory',
-                        ),
-                    ),
-                ),
-            ),
-        )
-    );
-    $routerDiConfig->configure($di);
-
-    call_user_func_array($moduleTestCaseClassname.'::setLocator', array($di));
 }
 
-// When this is in global scope, PHPUnit catches exception:
-// Exception: Zend\Stdlib\PriorityQueue::serialize() must return a string or NULL
-unset($moduleManager, $sharedEvents);
+if (! isset($loader)) {
+    throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
+}
+
+/* @var $loader \Composer\Autoload\ClassLoader */
+$loader->add('LosLicenseTest\\', __DIR__);
+
+if (file_exists(__DIR__ . '/TestConfiguration.php')) {
+    $config = require __DIR__ . '/TestConfiguration.php';
+} else {
+    $config = require __DIR__ . '/TestConfiguration.php.dist';
+}
+
+ServiceManagerTestCase::setConfiguration($config);
+unset($files, $file, $loader, $config);
